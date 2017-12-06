219b9d5fe24c7516952e1d4b77814d8794b7e4d2
Shweta Oak <oaks9599@gmail.com>
2017-12-06 16:01:42 -0500

    solution is drawn in red, problem in black

--- draw.py
+++ draw.py
@@ -10,18 +10,19 @@ import turtle
 
 
 ninja = turtle.Turtle()
-ninja.speed(1) #modify the speed 
+ninja.speed(1) #modify the speed
 
 
+problem = "004300209005009001070060043006002087190007400050083000600000105003508690042910300"
+solution = "864371259325849761971265843436192587198657432257483916689734125713528694542916378"
 
-sudoku = "075000096030100075020970004740000308089000460306000017200063080590008020860000540"
-
-
-def draw(num,initial_x,initial_y):
+def draw(num,initial_x,initial_y, c):
     if num == "1":
         ninja.penup()
         ninja.setposition(initial_x + 10,initial_y + 15)
+        ninja.color(c)
         ninja.pendown()
+
         ninja.right(90)
         ninja.forward(10)
         ninja.left(90)
@@ -30,6 +31,7 @@ def draw(num,initial_x,initial_y):
     elif num == "2":
         ninja.penup()
         ninja.setposition(initial_x + 5,initial_y + 15)
+        ninja.color(c)
         ninja.pendown()
         ninja.forward(10)
         ninja.right(90)
@@ -45,6 +47,7 @@ def draw(num,initial_x,initial_y):
     elif num == "3":
         ninja.penup()
         ninja.setposition(initial_x + 5,initial_y + 15)
+        ninja.color(c)
         ninja.pendown()
         ninja.forward(10)
         ninja.right(90)
@@ -62,6 +65,7 @@ def draw(num,initial_x,initial_y):
     elif num == "4":
         ninja.penup()
         ninja.setposition(initial_x + 15,initial_y + 15)
+        ninja.color(c)
         ninja.pendown()
         ninja.left(45)
         ninja.backward(10)
@@ -78,6 +82,7 @@ def draw(num,initial_x,initial_y):
     elif num == "5":
         ninja.penup()
         ninja.setposition(initial_x + 15,initial_y + 15)
+        ninja.color(c)
         ninja.pendown()
         ninja.backward(10)
         ninja.right(90)
@@ -94,6 +99,7 @@ def draw(num,initial_x,initial_y):
     elif num == "6":
         ninja.penup()
         ninja.setposition(initial_x + 15,initial_y + 15)
+        ninja.color(c)
         ninja.pendown()
         ninja.backward(10)
         ninja.right(90)
@@ -109,8 +115,12 @@ def draw(num,initial_x,initial_y):
 
     elif num == "7":
         ninja.penup()
+
         ninja.setposition(initial_x + 5,initial_y + 15)
+        ninja.color(c)
+
         ninja.pendown()
+
         ninja.forward(10)
         ninja.left(45)
         ninja.backward(14)
@@ -120,6 +130,7 @@ def draw(num,initial_x,initial_y):
     elif num == "8":
         ninja.penup()
         ninja.setposition(initial_x + 15,initial_y + 15)
+        ninja.color(c)
         ninja.pendown()
         ninja.backward(10)
         ninja.right(90)
@@ -142,6 +153,7 @@ def draw(num,initial_x,initial_y):
     elif num == "9":
         ninja.penup()
         ninja.setposition(initial_x + 5,initial_y + 5)
+        ninja.color(c)
         ninja.pendown()
         ninja.forward(10)
         ninja.left(90)
@@ -157,6 +169,7 @@ def draw(num,initial_x,initial_y):
     else:
         ninja.penup()
         ninja.setposition(initial_x+5, initial_y+10)
+        ninja.color(c)
         ninja.pendown()
 
         ninja.forward(10)
@@ -173,10 +186,13 @@ while j > -90:
         initial_x= i;
         initial_y = j;
 
-        number = sudoku[index]
+        number = solution[index]
+        if(str(solution[index]) == str(problem[index])):
+            c = "black"
+        else:
+            c = "red"
 
-
-        draw(number, initial_x, initial_y)
+        draw(number, initial_x, initial_y, c)
 
         index = index + 1
         i = i+ 20
