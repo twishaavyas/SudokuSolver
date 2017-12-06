56463c4db5f57024f97f2b966f5bc2bc796ce539
Shweta Oak <oaks9599@gmail.com>
2017-12-06 16:17:25 -0500

    changed pensize

--- draw.py
+++ draw.py
@@ -13,9 +13,8 @@ def draw(problem, solution):
 
     ninja = turtle.Turtle()
     ninja.speed(1) #modify the speed
+    ninja.pensize(2)
 
-    print problem
-    print solution
     # problem = "004300209005009001070060043006002087190007400050083000600000105003508690042910300"
     # solution = "864371259325849761971265843436192587198657432257483916689734125713528694542916378"
 
